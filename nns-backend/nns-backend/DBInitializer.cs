795eb6fb14391ff840ce844d6aa1baa2745c4989
Le Quoc Uy <lequocuyit@gmail.com>
2024-10-27 18:13:04 +0700

    fix seed data

--- nns-backend/nns-backend/DBInitializer.cs
+++ nns-backend/nns-backend/DBInitializer.cs
@@ -10,41 +10,6 @@ namespace nns_backend
             //Check if admin is not have in db add admin
             if (context.Users.Where(u => u.UserName == "admin").FirstOrDefault() == null)
             {
-                //Add Admin
-                var user = new User
-                {
-                    UserName = "admin",
-                    Email = "admin@gmail.com"
-                };
-                await userManager.CreateAsync(user, "123456");
-                await userManager.AddToRoleAsync(user, "ADMIN");
-            }
-
-            //Add Blogs
-            if (!context.Blogs.Any())
-            {
-                var blogs = new List<Blog>
-                {
-                    new Blog
-                    {
-                        Title = "Bèo hoa dâu – Tiềm năng lớn trong sản xuất nông nghiệp",
-                        Caption = "Bèo hoa dâu có nhiều tiềm năng lớn trong nông nghiệp như: giảm phát thải khí nhà kính, giảm chi phí về phân bón, sản xuất nông sản sạch, xanh… Mặc dù tiềm năng rất lớn nhưng bèo hoa dâu chưa thật sự đi vào sản xuất lúa bền vững. ",
-                        YoutubeLink = "https://www.youtube.com/watch?v=1hwRteliFaw"
-                    },
-                    new Blog
-                    {
-                        Title = "Tỷ phú nuôi lợn \"không cần tắm\", bỏ túi tiền tỷ, tránh dịch bệnh",
-                        Caption = "VTC16 | Tỷ phú nuôi lợn \"không cần tắm\", bỏ túi tiền tỷ, tránh dịch bệnh. Ông Nguyễn Văn Sợi, Hà Nội chăn nuôi lợn không cần tắm, ông dùng đệm lót sinh học và men vi sinh, bí quyết nào giúp ông Sợi tránh được bệnh dịch tả lợn Châu Phi và kiếm tiền tỷ?",
-                        YoutubeLink = "https://www.youtube.com/watch?v=4B6pQPEh_X8"
-                    }
-                };
-                await context.Blogs.AddRangeAsync(blogs);
-                await context.SaveChangesAsync();
-            }
-
-            //Add Roles
-            try
-            {
                 if (!context.Roles.Any())
                 {
                     var roles = new List<Role>
@@ -60,30 +25,82 @@ namespace nns_backend
                         NormalizedName = "AGENT"
                     }
                 };
-                    await context.Roles.AddRangeAsync(roles);
-                    await context.SaveChangesAsync();
 
-                    //Add Agent
-                    var user1 = new User
+                    //Add Admin
+                    var user = new User
                     {
-                        UserName = "agent1",
-                        Email = "agent1@gmail.com"
+                        UserName = "admin",
+                        Email = "admin@gmail.com"
                     };
-                    await userManager.CreateAsync(user1, "123456");
-                    await userManager.AddToRoleAsync(user1, "AGENT");
-
-                    var user2 = new User
-                    {
-                        UserName = "agent2",
-                        Email = "agent2@gmail.com"
-                    };
-                    await userManager.CreateAsync(user2, "123456");
-                    await userManager.AddToRoleAsync(user2, "AGENT");
+                    await userManager.CreateAsync(user, "123456");
+                    await userManager.AddToRoleAsync(user, "ADMIN");
                 }
-            }
-            catch (Exception e)
-            {
-                throw e;
+
+                //Add Blogs
+                if (!context.Blogs.Any())
+                {
+                    var blogs = new List<Blog>
+                {
+                    new Blog
+                    {
+                        Title = "Bèo hoa dâu – Tiềm năng lớn trong sản xuất nông nghiệp",
+                        Caption = "Bèo hoa dâu có nhiều tiềm năng lớn trong nông nghiệp như: giảm phát thải khí nhà kính, giảm chi phí về phân bón, sản xuất nông sản sạch, xanh… Mặc dù tiềm năng rất lớn nhưng bèo hoa dâu chưa thật sự đi vào sản xuất lúa bền vững. ",
+                        YoutubeLink = "https://www.youtube.com/watch?v=1hwRteliFaw"
+                    },
+                    new Blog
+                    {
+                        Title = "Tỷ phú nuôi lợn \"không cần tắm\", bỏ túi tiền tỷ, tránh dịch bệnh",
+                        Caption = "VTC16 | Tỷ phú nuôi lợn \"không cần tắm\", bỏ túi tiền tỷ, tránh dịch bệnh. Ông Nguyễn Văn Sợi, Hà Nội chăn nuôi lợn không cần tắm, ông dùng đệm lót sinh học và men vi sinh, bí quyết nào giúp ông Sợi tránh được bệnh dịch tả lợn Châu Phi và kiếm tiền tỷ?",
+                        YoutubeLink = "https://www.youtube.com/watch?v=4B6pQPEh_X8"
+                    }
+                };
+                    await context.Blogs.AddRangeAsync(blogs);
+                    await context.SaveChangesAsync();
+                }
+
+                //Add Roles
+                try
+                {
+                    if (!context.Roles.Any())
+                    {
+                        var roles = new List<Role>
+                {
+                    new Role
+                    {
+                        Name = "Admin",
+                        NormalizedName = "ADMIN"
+                    },
+                    new Role
+                    {
+                        Name = "Agent",
+                        NormalizedName = "AGENT"
+                    }
+                };
+                        await context.Roles.AddRangeAsync(roles);
+                        await context.SaveChangesAsync();
+
+                        //Add Agent
+                        var user1 = new User
+                        {
+                            UserName = "agent1",
+                            Email = "agent1@gmail.com"
+                        };
+                        await userManager.CreateAsync(user1, "123456");
+                        await userManager.AddToRoleAsync(user1, "AGENT");
+
+                        var user2 = new User
+                        {
+                            UserName = "agent2",
+                            Email = "agent2@gmail.com"
+                        };
+                        await userManager.CreateAsync(user2, "123456");
+                        await userManager.AddToRoleAsync(user2, "AGENT");
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
             }
         }
     }
